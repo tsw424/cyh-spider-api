0aac720dc5ba15d3ca0b4d43722f4a6ebb1e7ca5
chaterhower <1161638319@qq.com>
2018-11-19 16:17:51 +0800

    代码覆盖率

--- src/test/java/cn/zero/spider/SpiderApplicationTests.java
+++ src/test/java/cn/zero/spider/SpiderApplicationTests.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpiderApplication.class, properties = "application-sit.properties"
+@SpringBootTest(classes = SpiderApplication.class, properties = "application-ci.properties"
         , webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SpringBootApplication
 @MapperScan({"cn.zero.spider.dao"})
